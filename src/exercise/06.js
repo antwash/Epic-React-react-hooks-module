9855e9ab150a30561709f0a1917a19c8c4f7cb2f
Anthony Washington <awashington251@gmail.com>
2022-05-08 20:45:32 -0400

    exercise 6-1: handle errors

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -7,6 +7,7 @@ import { fetchPokemon, PokemonDataView, PokemonInfoFallback, PokemonForm } from 
 
 function PokemonInfo({ pokemonName }) {
   const [pokemon, setPokemon] = React.useState(null)
+  const [error, setError] = React.useState(null)
 
   React.useEffect(() => {
     if (!pokemonName) {
@@ -14,13 +15,24 @@ function PokemonInfo({ pokemonName }) {
     }
 
     setPokemon(null)
+    setError(null)
     fetchPokemon(pokemonName).then(pokemon => {
       setPokemon(pokemon)
+    }, error => {
+      setError(error)
     } )
   }, [pokemonName])
 
   if (!pokemonName) {
     return <span>Submit a pokemon</span>
+  }
+
+  if (error) {
+    return (
+      <div role="alert">
+        There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
   }
 
   if (!pokemon) {
